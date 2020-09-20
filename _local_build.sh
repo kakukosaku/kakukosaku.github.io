41d1b4cf7b6812aa2eb6f3922683ba321e390d4e
kaku <scugjs@gmail.com>
2020-09-20 10:19:25 +0800

    [update] 更新blog细节

--- _local_build.sh
+++ _local_build.sh
@@ -6,7 +6,7 @@ rubyenv="$HOME/.gem/ruby/2.6.0/bin"
 serve() {
     echo "serve pages locally!"
     echo "Goto: \n\thttp://localhost:4000/"
-    $rubyenv/bundle exec $rubyenv/jekyll serve
+    $rubyenv/bundle exec $rubyenv/jekyll serve -t
 }
 
 install() {
